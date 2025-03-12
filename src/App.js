7c0cb49a6d45fde3bbb893360059bdf05753cc2a
Zandile Gebuza <2562617@students.wits.ac.za>
2025-03-12 12:33:17 +0200

    React Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-export default function App(){
+import Navbar from "./Practice/Week 5/Examples/Components/Navbar";
+
+import Home from "./Practice/Week 5/Examples/Pages/Home";
+import About from "./Practice/Week 5/Examples/Pages/About";
+import Products from "./Practice/Week 5/Examples/Pages/Products";
+import NotFoundPage from "./Practice/Week 5/Examples/Pages/NotFoundPage.jsx";
+
+function App(){
   return (
-    <div className="App">
-
-    </div>
+   <BrowserRouter>
+   <Navbar />
+   <Routes>
+    {/*when this url is visited, what page would you like to see. 
+    This is because in react, we don't have pages but we have components*/}
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/products" element={<Products />} />
+   
+   {/*This is a catch-all for any routes that have not been specified*/}
+    <Route path="*" element={<NotFoundPage />}/>
+    </Routes>
+   </BrowserRouter>
   );
 }
+
+export default App;
---
+++ src/Practice/Week 5/Examples/Pages/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Products() {
+ 
+    return (
+    <div>
+        <h3>This is the Products Page</h3>
+    </div>
+  );
+}
+
+export default Products;
