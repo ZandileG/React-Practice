da08aad94de03fbd62e46c508890320a8ff40472
Zandile Gebuza <2562617@students.wits.ac.za>
2025-03-17 22:59:55 +0200

    Exercise 1 done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./Practice/Week 6/Exercise 2/NavBar";
 
-import PersonalDetails from "./Practice/Week 5/Exercise 2/PersonalDetails";
-import AddressDetails from "./Practice/Week 5/Exercise 2/AddressDetails";
-import Confirmation from "./Practice/Week 5/Exercise 2/Confirmation";
-import NotFoundPage from "./Practice/Week 5/Examples/Example 1/Pages/NotFoundPage"
+/*
+import Grid from "./Practice/Week 6/Exercise 3/Grid";
+import ContentCard from "./Practice/Week 6/Exercise 3/ContentCard";
+*/
 
 function App(){
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<PersonalDetails />} />
-    <Route path="/address" element={<AddressDetails />} />
-    <Route path="/confirmation" element={<Confirmation />} />
-    <Route path="*" element={<NotFoundPage />} />
-z   </Routes>
-   </BrowserRouter>
+  <div>
+    <NavBar />
+    {/*
+    <Grid />
+    <ContentCard /> 
+    */}
+  </div>
   );
 }
 
--- src/Practice/Week 6/Exercise 1/ProfileCard.jsx
+++ src/Practice/Week 6/Exercise 1/ProfileCard.jsx
@@ -4,9 +4,9 @@ import "./ProfileCard.css"
 function ProfileCard() {
   return (
     <div className="card">
-      <img src="profile-pic.jpg" alt="Profile" className="profileImg" />
+      <img src="https://static.vecteezy.com/system/resources/previews/029/271/069/non_2x/avatar-profile-icon-in-flat-style-female-user-profile-illustration-on-isolated-background-women-profile-sign-business-concept-vector.jpg" alt="Profile" className="profileImg" />
       <h2 className="profileName">Jane Doe</h2>
-      <p className="profileBio">Frontend Developer who loves to create beautiful and performant web applications.</p>
+      <p className="profileBio">Front-end Developer who loves to create beautiful and performant web applications.</p>
     </div>
   );
 };
